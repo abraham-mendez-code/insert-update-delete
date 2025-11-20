5f253dc5ff24c017e8dc73c83a875cef789ee03d
Abraham <abraham.me001@gmail.com>
2025-11-20 11:13:24 -0500

    feat(sql): add script to insert new product linked to new supplier

---
+++ 03_show_products.sql
@@ -0,0 +1,11 @@
+# List all products and their suppliers.
+use northwind;
+
+select
+	ProductID
+    ,ProductName
+    ,s.SupplierID
+    ,CompanyName
+from
+	products p
+    join suppliers s on s.SupplierID = p.SupplierID;
