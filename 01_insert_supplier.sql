1341be21a7ce90141c700c8aaaf3ae54d5d05a83
Abraham <abraham.me001@gmail.com>
2025-11-20 11:12:55 -0500

    feat(sql): add script to insert new supplier into Suppliers table

---
+++ 01_insert_supplier.sql
@@ -0,0 +1,6 @@
+# Add a new supplier.
+use northwind;
+
+insert into 
+	suppliers(CompanyName, ContactName, ContactTitle, Address, City, PostalCode, Country, Phone)
+values('YearUpUnited', 'John Doe', 'Student', '85 Broad St 6th Floor', 'New York', 10004, 'United States', '(212) 785-3340');
